cf0e21de2ff6b65e1eb8dce22f530dd875bc714f
Seven719 <ilie.andronie@gmail.com>
2023-10-31 10:47:15 +0000

    Added playRound function and game function
    made a loop inside game function to repeat game 5 times

--- javascript.js
+++ javascript.js
@@ -5,9 +5,23 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {	
+		if (playerChoice === computerChoice) {
+        return "It's a tie!";
+    }
+		else if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
+        return `You win! ${playerChoice} beats ${computerChoice}.`;
+    } 
+		else {
+        return `You lose! ${computerChoice} beats ${playerChoice}.`;
+    }	
 }
 
-const playerChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
-const computerChoice = getComputerChoice();
+function game() {
+	for (let i = 0; i < 5; i++) {
+		const playerChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
+		const computerChoice = getComputerChoice();
+		console.log(playRound(playerChoice, computerChoice));
+	}
+}
 
-console.log(playRound(playerChoice, computerChoice));
+game();
